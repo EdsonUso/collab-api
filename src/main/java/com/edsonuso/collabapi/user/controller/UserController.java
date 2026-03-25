208d38c4836c09809a7178475242fca825919427
EdsonUso <edson.cruz@sptech.school>
2026-03-25 20:29:27 -0300

    :bug: fix - resolver bug de mapeamento correto para check-username

--- src/main/java/com/edsonuso/collabapi/user/controller/UserController.java
+++ src/main/java/com/edsonuso/collabapi/user/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 @Tag(name = "Users", description = "Endpoints de usuários")
 public class UserController {
 
